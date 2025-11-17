d53fd33e24578c12cc236b6189e5838abd9b184e
Mazun Volodimir <mazunvolodimir777@gmail.com>
2025-11-18 00:12:13 +0200

    finish work

--- src/server.js
+++ src/server.js
@@ -1,3 +1,68 @@
-const message = "Hello world";
+import express from 'express';
+import cors from 'cors';
+import pino from 'pino-http';
+import 'dotenv/config';
 
-console.log(message);
+const app = express();
+const PORT = process.env.PORT ?? 3000; //Використовуємо значення з .env або дефолтний порт 3000
+
+app.use(express.json()); // Middleware для парсингу JSON
+app.use(cors()); // Дозволяє запити з будь-яких джерел
+app.use(
+  pino({
+    level: 'info',
+    transport: {
+      target: 'pino-pretty',
+      options: {
+        colorize: true,
+        translateTime: 'HH:MM:ss',
+        ignore: 'pid,hostname',
+        messageFormat:
+          '{req.method} {req.url} {res.statusCode} - {responseTime}ms',
+        hideObject: true,
+      },
+    },
+  }),
+);
+
+// GET-запит до кореневого маршруту "/"
+app.get('/', (req, res) => {
+  res.status(200).json({ message: 'Collection of Notes, Welcome' });
+});
+
+// Список усіх нотаток
+app.get('/notes', (req, res) => {
+  res.status(200).json({ message: 'Retrieved all notes' });
+});
+
+// Конкретна нотатка за id
+app.get('/notes/:noteId', (req, res) => {
+  const { noteId } = req.params;
+  res.status(200).json({ message: `Retrieved note with ID: ${noteId}` });
+});
+
+// Маршрут для тестування middleware помилки
+app.get('/test-error', () => {
+  throw new Error('Simulated server error');
+});
+
+// Обробка неіснуючих маршрутів
+app.use((req, res) => {
+  res.status(404).json({ message: 'Route not found' });
+});
+
+// Middleware для обробки помилок
+app.use((err, req, res, next) => {
+  console.error(err);
+  const isProd = process.env.NODE_ENV === 'production';
+  res.status(500).json({
+    message: isProd
+      ? 'Something went wrong. Please try again later.'
+      : err.message,
+  });
+});
+
+// Запуск сервера
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
