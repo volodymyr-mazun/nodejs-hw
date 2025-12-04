a7c41c924f10092c23a7f4d1f0141aa73a5f9c69
Mazun Volodimir <mazunvolodimir777@gmail.com>
2025-12-04 22:44:01 +0200

    validation request

---
+++ src/validations/notesValidation.js
@@ -0,0 +1,70 @@
+import { Joi, Segments } from 'celebrate';
+import { isValidObjectId } from 'mongoose';
+
+export const createNoteSchema = {
+  [Segments.BODY]: Joi.object({
+    title: Joi.string().min(3).max(20).required().messages({
+      'string.base': 'Title must be a string',
+      'string.min': 'Title should have at least {#limit} characters',
+      'string.max': 'Title should have at most {#limit} characters',
+      'any.required': 'Title is required',
+    }),
+    content: Joi.string().min(10).max(50).required().messages({
+      'string.base': 'Content must be a string',
+      'string.min': 'Content should have at least {#limit} characters',
+      'string.max': 'Content should have at least {#limit} characters',
+      'any.required': 'Content is required',
+    }),
+    tag: Joi.string()
+      .valid(
+        'Work',
+        'Personal',
+        'Meeting',
+        'Shopping',
+        'Ideas',
+        'Travel',
+        'Finance',
+        'Health',
+        'Important',
+        'Todo',
+      )
+      .required()
+      .messages({
+        'any.only':
+          'Tag must be one of: work, personal, meeting, shopping, ideas, travel, finance, health, important, todo',
+        'any.required': 'Tag is required',
+      }),
+  }),
+};
+
+const objectIdValidator = (value, helpers) => {
+  return !isValidObjectId(value) ? helpers.message('Invalid id format') : value;
+};
+
+export const noteIdSchema = {
+  [Segments.PARAMS]: Joi.object({
+    noteId: Joi.string().custom(objectIdValidator).required(),
+  }),
+};
+
+export const updateNoteSchema = {
+  [Segments.PARAMS]: Joi.object({
+    noteId: Joi.string().custom(objectIdValidator).required(),
+  }),
+  [Segments.BODY]: Joi.object({
+    title: Joi.string().min(3).max(20),
+    content: Joi.string().min(10).max(50),
+    gender: Joi.string().valid(
+      'Work',
+      'Personal',
+      'Meeting',
+      'Shopping',
+      'Ideas',
+      'Travel',
+      'Finance',
+      'Health',
+      'Important',
+      'Todo',
+    ),
+  }).min(1),
+};
